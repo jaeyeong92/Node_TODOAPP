d49bf6757dbc913a6f944a2902488bb13cf48dd0
jaeyeong92 <jjy4041@gmail.com>
2023-04-04 02:56:33 +0900

    Node-Express Prac
    20230404 Commit

--- server.js
+++ server.js
@@ -48,12 +48,22 @@ app.get('/write', function(req, res){
   res.sendFile(__dirname + '/write.html');
 });
 
+// DB에 글 저장
 app.post('/add', function(req, res){
   res.send('전송완료');
-  console.log(req.body.date);
-  console.log(req.body.title);
-  db.collection('post').insertOne({_id : 4, 제목 : req.body.title, 날짜 : req.body.date},function(에러, 결과){
-    console.log('저장완료');
+  // 서버로 보낸 정보(req) 확인
+  console.log(req.body);
+  // 게시물갯수 counter 찾기
+  db.collection('counter').findOne({name : '게시물갯수'}, function(error, result){
+    let 총게시물갯수 = result.totalPost;
+    // 글 저장
+    db.collection('post').insertOne({_id : 총게시물갯수 + 1, 제목 : req.body.title, 날짜 : req.body.date},function(에러, 결과){
+      console.log('DB에 글 저장완료');
+      // counter collection의 totalPost 도 +1 증가             operator : $set(변경), $inc(증가)
+      db.collection('counter').updateOne({name : '게시물갯수'}, { $inc : {totalPost : 1} }, function(error, result){
+        if(error) {return console.log(error)};
+      })
+    });
   });
 });
 
@@ -62,8 +72,19 @@ app.get('/list', function(req, res){
 
   // DB에 저장된 post라는 collection의 모든데이터를 꺼내기
   db.collection('post').find().toArray(function(error, result){
-    console.log(result);
+    console.log([result]);
     res.render('list.ejs', {posts : result});
+  });
+});
+
+
+// DELETE 요청
+app.delete('/delete', function(req, res){
+  console.log(req.body);
+  req.body._id = parseInt(req.body._id);
+  // req.body에 담겨온 게시물번호를 가진 글을 DB에서 찾아서 삭제
+  db.collection('post').deleteOne(req.body, function(error, result){
+    console.log('삭제완료');
   });
 });
 
